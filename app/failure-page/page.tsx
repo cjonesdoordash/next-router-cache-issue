1091687a9cc94b6edcec4444b712584ebb4a738d
Cody Jones <cody.jones@doordash.com>
2024-04-17 15:00:27 -0700

    Error reproduction

---
+++ app/failure-page/page.tsx
@@ -0,0 +1,4 @@
+/** Add your relevant code here for the issue to reproduce */
+export default async function Home() {
+  throw new Error('failure')
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,24 @@
+import Link from 'next/link'
+
+export const dynamic = 'force-dynamic'
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <div>
+          <Link prefetch={false} href={'/success-page'}>
+            Successful Page
+          </Link>
+        </div>
+        <div>
+          <Link prefetch={false} href={'/failure-page'}>
+            Failure Page
+          </Link>
+        </div>
+        <div>{children}</div>
+      </body>
     </html>
-  );
+  )
 }
